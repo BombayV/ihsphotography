98ff3cc18b8924f3b88e941a10819c657a8bd72a
BombayV <OMGmsebastian@gmail.com>
2022-03-27 19:43:40 -0400

    test

--- pages/register.vue
+++ pages/register.vue
@@ -102,3 +102,7 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400;500;600;700;800;900&family=Playfair+Display:wght@800&display=swap');
+</style>
